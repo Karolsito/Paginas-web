5b5ac613092206ab798ff225d691ae6b21d87f8e
Karolsito <karolsilva@live.com.ar>
2025-09-16 17:42:22 -0300

    Add files via upload

---
+++ JSON/RecibosCobranza.js
@@ -0,0 +1,15 @@
+var textoJSONRecibos = '{"RecibosDeCobranza":' +
+'[' +
+  '{"Cod_cli":"00123","Razon_social":"Distribuidora El Sol S.A.","NroDeRecibo":"RC-2025-0001","Fecha_cobranza":"2025-09-16","Importe_cobranza":125000.50,"MedioDePago":{"cod-medio":"05","Descripcion":"Transferencia"},"Pdf_comprobante":"https://www.distribuidoraelsol.com/comprobantes/RC-2025-0001.pdf"},' +
+  '{"Cod_cli":"00124","Razon_social":"Ferretería El Tornillo","NroDeRecibo":"RC-2025-0002","Fecha_cobranza":"2025-09-17","Importe_cobranza":85000.00,"MedioDePago":{"cod-medio":"02","Descripcion":"Efectivo"},"Pdf_comprobante":"comprobante_RC-2025-0002.pdf"}' +
+']' +
+'}';
+var textoJSONMediosDePago = '{"MediosDePago":' +
+'[' +
+  '{"cod-medio":"01","Descripcion":"Cheque"},' +
+  '{"cod-medio":"02","Descripcion":"Efectivo"},' +
+  '{"cod-medio":"03","Descripcion":"Tarjeta de crédito"},' +
+  '{"cod-medio":"04","Descripcion":"Tarjeta de débito"},' +
+  '{"cod-medio":"05","Descripcion":"Transferencia"}' +
+']' +
+'}';
