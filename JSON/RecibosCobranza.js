01f0e405f2a6367fcb59a77b6636408f46a652a7
Karolsito <karolsilva@live.com.ar>
2025-09-16 19:45:42 -0300

    Update RecibosCobranza.js

--- JSON/RecibosCobranza.js
+++ JSON/RecibosCobranza.js
@@ -1,7 +1,7 @@
 var textoJSONRecibos = '{"RecibosDeCobranza":' +
 '[' +
-  '{"Cod_cli":"00123","Razon_social":"Distribuidora El Sol S.A.","NroDeRecibo":"RC-2025-0001","Fecha_cobranza":"2025-09-16","Importe_cobranza":125000.50,"MedioDePago":{"cod-medio":"05","Descripcion":"Transferencia"},"Pdf_comprobante":"https://www.distribuidoraelsol.com/comprobantes/RC-2025-0001.pdf"},' +
-  '{"Cod_cli":"00124","Razon_social":"Ferretería El Tornillo","NroDeRecibo":"RC-2025-0002","Fecha_cobranza":"2025-09-17","Importe_cobranza":85000.00,"MedioDePago":{"cod-medio":"02","Descripcion":"Efectivo"},"Pdf_comprobante":"comprobante_RC-2025-0002.pdf"}' +
+  '{"Cod_cli":"00123","Razon_social":"Distribuidora El Sol S.A.","NroDeRecibo":"RC-2025-0001","Fecha_cobranza":"2025-09-16","Importe_cobranza":125000.50,"MedioDePago":"05","Pdf_comprobante":"https://www.distribuidoraelsol.com/comprobantes/RC-2025-0001.pdf"},' +
+  '{"Cod_cli":"00124","Razon_social":"Ferretería El Tornillo","NroDeRecibo":"RC-2025-0002","Fecha_cobranza":"2025-09-17","Importe_cobranza":85000.00,"MedioDePago":"02","Pdf_comprobante":"comprobante_RC-2025-0002.pdf"}' +
 ']' +
 '}';
 var textoJSONMediosDePago = '{"MediosDePago":' +
@@ -13,3 +13,4 @@ var textoJSONMediosDePago = '{"MediosDePago":' +
   '{"cod-medio":"05","Descripcion":"Transferencia"}' +
 ']' +
 '}';
+
